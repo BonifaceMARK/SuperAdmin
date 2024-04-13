83df90d1e26ab3c5f1b84e707fffba64811863c9
Binn12345 <arvincejalbo18@gmail.com>
2024-04-13 18:09:00 +0800

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,12 @@ use App\Http\Controllers\SubAdminController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
-<<<<<<< HEAD
 
 
 USE App\Http\Controllers\PendingController;
 USE App\Http\Controllers\ClientController;
 USE App\Http\Controllers\Reports;
-=======
 use App\Http\Controllers\fms5Controller;
->>>>>>> b896e76625e61fd916b31c7e65490c0953d3a07e
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -80,13 +77,11 @@ Route::get('/fms5stan', [fms5Controller::class, 'fms5standards'])->name('standar
     Route::get('/manage-role',      [SuperAdminController::class,'manageRole'])->name('manageRole');
     Route::post('/update-role',     [SuperAdminController::class,'updateRole'])->name('updateRole');
     Route::get('/users/{id}/edit',  [SuperAdminController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{id}',       [SuperAdminController::class, 'updateRole'])->name('users.update');
-    Route::delete('/users/{id}',    [SuperAdminController::class, 'destroy'])->name('users.destroy');
+    Route::put('/users/{id}',           [SuperAdminController::class, 'updateRole'])->name('users.update');
+    Route::delete('/users/{id}',        [SuperAdminController::class, 'destroy'])->name('users.destroy');
 
 
 
-
-    
     Route::GET('/home',                            [App\Http\Controllers\SuperadminController::class, 'home'])               ->name('home');
     Route::GET('/user-management',                 [App\Http\Controllers\SuperadminController::class, 'usermanagement'])     ->name('user-management'); 
     Route::GET('/depreciation-calculation',        [App\Http\Controllers\SuperadminController::class, 'depreciation'])       ->name('user-depreciation'); 
@@ -120,6 +115,8 @@ Route::get('/fms5stan', [fms5Controller::class, 'fms5standards'])->name('standar
     Route::GET('/getassettransaction',             [App\Http\Controllers\SuperadminController::class, 'getassettransaction'])          ->name('getassettransaction');
 
     ROUTE::POST('/reportidx',                      [App\Http\Controllers\SuperadminController::class, 'reportidx'])                    ->name('reportidx');
+
+
 
 
     Route::GET('/getdepreciation',                 [App\Http\Controllers\SuperadminController::class, 'getdepreciation'])              ->name('getdepreciation'); 
@@ -171,18 +168,28 @@ Route::group(['prefix' => 'user','middleware'=>['web','isUser']],function(){
 
 
 
+    Route::GET('/getAssetInventory',               [App\Http\Controllers\SuperadminController::class, 'getAssetInventory'])            ->name('mgetAssetInventory');  
+    Route::GET('/depreciation',                    [App\Http\Controllers\SuperadminController::class, 'depreciation'])                 ->name('mdepreciation'); 
+    Route::GET('/tracking',                        [App\Http\Controllers\SuperadminController::class, 'tracking'])                     ->name('mtracking');
+
+    // Route::get('/muser-management',                 [App\Http\Controllers\SuperadminController::class, 'managerusermanagement'])     ->name('muser-management'); 
 
 
-    // basic user asset manager
-    Route::GET('/getAssetInventory',     [App\Http\Controllers\SuperadminController::class, 'getAssetInventory'])         ->name('mgetAssetInventory');  
-    Route::GET('/depreciation',          [App\Http\Controllers\SuperadminController::class, 'depreciation'])              ->name('mdepreciation'); 
-    Route::GET('/tracking',              [App\Http\Controllers\SuperadminController::class, 'tracking'])                   ->name('mtracking');
+
 
     // Process
-    Route::GET('/getdepreciation',       [App\Http\Controllers\SuperadminController::class, 'getdepreciation'])           ->name('mgetdepreciation');    
-    Route::POST('/recompute',            [App\Http\Controllers\RecomputeAssetController::class, 'recdepreciation'])       ->name('mrecompute');
+    Route::GET('/getdepreciation',                 [App\Http\Controllers\SuperadminController::class, 'getdepreciation'])              ->name('mgetdepreciation');    
+    Route::POST('/recompute',                      [App\Http\Controllers\RecomputeAssetController::class, 'recdepreciation'])          ->name('mrecompute');
+
+
+
+
+
+
+
     Route::GET('/generate-report',       [App\Http\Controllers\ReportAccessController::class, 'generateReport'])          ->name('ugenerateReport');
     Route::POST('/notice',               [App\Http\Controllers\UniversalProcess::class, 'isNotice'])                      ->name('unotice');
+
     
     // view blade
     Route::GET('/home',                  [App\Http\Controllers\UniversalController::class, 'index'])                      ->name('uhome');
@@ -193,14 +200,11 @@ Route::group(['prefix' => 'user','middleware'=>['web','isUser']],function(){
     Route::GET('/report',                [App\Http\Controllers\UniversalController::class, 'report'])                     ->name('ureport');
     Route::GET('/profile',               [App\Http\Controllers\UniversalController::class, 'profile'])                    ->name('uprofile');
     Route::GET('/help',                  [App\Http\Controllers\UniversalController::class, 'customerservice'])            ->name('uhelp');
-
-
 });
 
 
 
 
-<<<<<<< HEAD
 
 
 // ********** F3 Routes *********
@@ -213,5 +217,3 @@ Route::get('/fix-asset-reports',        [Reports::class, 'fixass'])->name('fixas
 Route::get('/f3/tax-management-reports',   [Reports::class, 'taxfin'])->name('taxfin')->middleware(['auth']);
 
 Route::get('/f3/bank',                     [Reports::class, 'bank'])->name('bank')->middleware(['auth']);
-=======
->>>>>>> b896e76625e61fd916b31c7e65490c0953d3a07e
