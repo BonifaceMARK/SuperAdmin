e0cb0744022ef98b4d806bcb415425604e0ea772
Jake Bartolay <lilkraceboy@gmail.com>
2024-04-14 02:25:10 +0800

    icon :)
    1

--- resources/views/F10/inc/navbar.blade.php
+++ resources/views/F10/inc/navbar.blade.php
@@ -2,17 +2,17 @@
 {{-- <li class="nav-heading">Workflow Approval</li> --}}
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-reports1" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-menu-button-wide"></i><span>Vendor & Investment</span><i class="bi bi-chevron-down ms-auto"></i>
+          <i class="bi bi-people-fill"></i><span>Vendor & Investment</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="components-reports1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="{{ route('vendor.dashboard') }}">
-              <i class="bi bi-circle"></i><span>Vendor Management</span>
+              <i class="bi bi-person-lines-fill fs-5"></i><span>Vendor Management</span>
             </a>
           </li>
           <li>
             <a href="{{ route('investment.dashboard') }}">
-              <i class="bi bi-circle"></i><span>Investment Management</span>
+              <i class="bi bi-person-vcard-fill fs-5"></i><span>Investment Management</span>
             </a>
           </li><!-- End Forms Nav -->
 
