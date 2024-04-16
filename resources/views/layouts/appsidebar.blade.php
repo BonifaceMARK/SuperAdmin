c2b0240b7fab8d72f1d53a9e976a12cce2e212b8
Bootloader1z <matthewcrew1zx@gmail.com>
2024-04-16 10:08:03 +0800

    x

--- resources/views/layouts/appsidebar.blade.php
+++ resources/views/layouts/appsidebar.blade.php
@@ -306,7 +306,7 @@
   </li>
   <li class="nav-item">
     <a class="nav-link collapsed" class="nav-link " data-bs-target="#forms-nav"  href="fms3index">
-      <i class="bi bi-journal-text"></i><span style="font-size: 0.8rem;">Clients</span>
+      <i class="bi bi-journal-text"></i><span style="font-size: 0.8rem;">Workflow & Approval</span>
     </a>
   </li>
       <hr>
